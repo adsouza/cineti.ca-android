4fbe014090e01bd04b6758e0a39fd8b64746a4ea
Antonio D'souza <adsouza@gmail.com>
2010-05-14 15:20:18 -0400

    Replaced the autogenerated UI with a set of tabs.

--- src/ca/cineti/android/Main.java
+++ src/ca/cineti/android/Main.java
@@ -1,13 +1,25 @@
 package ca.cineti.android;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class Main extends Activity {
+public class Main extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TabHost tabHost = getTabHost(); // The activity TabHost
+        TabHost.TabSpec spec; // Reusable TabSpec for each tab
+        
+        // Initialise a TabSpec for each tab and add it to the TabHost.
+        spec = tabHost.newTabSpec("movies").setIndicator("Movies").setContent(R.id.txt1);
+        tabHost.addTab(spec);
+        spec = tabHost.newTabSpec("theatres").setIndicator("Theatres").setContent(R.id.txt2);
+        tabHost.addTab(spec);
+        
+        tabHost.setCurrentTab(0);
     }
 }
