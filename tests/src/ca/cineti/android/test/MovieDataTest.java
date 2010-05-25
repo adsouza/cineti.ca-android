0f724e4716c711c613ca8ae7e06cac461ebe3f77
William Lachance <wrlach@gmail.com>
2010-05-25 18:15:09 -0400

    Create new test project and put main project in new app/ subdirectory

---
+++ tests/src/ca/cineti/android/test/MovieDataTest.java
@@ -0,0 +1,23 @@
+package ca.cineti.android.test;
+
+import junit.framework.TestCase;
+
+public class MovieDataTest extends TestCase {
+
+	public MovieDataTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testMovieDataContextJSONObject() {
+		fail("Not yet implemented");
+	}
+
+}
